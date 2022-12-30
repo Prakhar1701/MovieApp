9f32d115a3af7476899ba55bbb4758db0b060ee6
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2022-12-30 12:27:43 +0530

    Using LazyColum and Showing Movies

--- app/src/main/java/prakhar/udemy/jetpackcompose/movieapp/MainActivity.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/movieapp/MainActivity.kt
@@ -3,14 +3,21 @@ package prakhar.udemy.jetpackcompose.movieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import prakhar.udemy.jetpackcompose.movieapp.ui.theme.MovieAppTheme
 
@@ -47,15 +54,46 @@ fun MainContent(
         "Avatar",
         "300",
         "Harry Potter",
-        "Life"
+        "Life",
+        "Happiness...",
+        "Prakhar",
+        "Shikhar"
     )
 ) {
 
     Column(modifier = Modifier.padding(12.dp)) {
         LazyColumn {
             items(items = movieList) {
-                Text(text = it)
+                MovieRow(movie = it)
             }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = 6.dp
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                elevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+            }
+            Text(text = movie)
         }
     }
 }
