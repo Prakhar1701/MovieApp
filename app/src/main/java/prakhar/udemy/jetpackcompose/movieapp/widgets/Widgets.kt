f78da87b9998017bd3abf46576d7b7d08e5e9738
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2022-12-30 23:33:05 +0530

    Used Coil to Add Images

--- app/src/main/java/prakhar/udemy/jetpackcompose/movieapp/widgets/Widgets.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/movieapp/widgets/Widgets.kt
@@ -1,25 +1,32 @@
 package prakhar.udemy.jetpackcompose.movieapp.widgets
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
+import coil.transform.CircleCropTransformation
 import prakhar.udemy.jetpackcompose.movieapp.model.Movie
+import prakhar.udemy.jetpackcompose.movieapp.model.getMovies
 
-
+@Preview
 @Composable
-fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}) {
+fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) {
     Card(
         modifier = Modifier
             .padding(4.dp)
@@ -40,9 +47,44 @@ fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}) {
                 shape = RectangleShape,
                 elevation = 4.dp
             ) {
-                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+
+//                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+//--------------------------------------------------------------------------------------------------
+/* From:
+  https://coil-kt.github.io/coil/compose/  */
+
+                val painter = rememberAsyncImagePainter(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(movie.images[0]) //Using Image-1 as poster as Poster link is not working :(
+                        .crossfade(true)
+                        .transformations(CircleCropTransformation())
+                        .size(Size.ORIGINAL) // Set the target size to load the image at.
+                        .build()
+                )
+
+                if (painter.state is AsyncImagePainter.State.Success) {
+                    // This will be executed during the first composition if the image is in the memory cache.
+                }
+
+                Image(
+                    painter = painter,
+                    contentDescription = "Movie Poster"
+                )
+//--------------------------------------------------------------------------------------------------
             }
-            Text(text = movie.title)
+            Column(modifier = Modifier.padding(4.dp)) {
+
+                Text(text = movie.title, style = MaterialTheme.typography.h6)
+                Text(
+                    text = "Director: ${movie.director}",
+                    style = MaterialTheme.typography.caption
+                )
+                Text(
+                    text = "Released: ${movie.year}",
+                    style = MaterialTheme.typography.caption
+                )
+
+            }
         }
     }
 }
