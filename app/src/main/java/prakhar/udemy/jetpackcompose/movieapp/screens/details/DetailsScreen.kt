55c28f0f6c6b48e3b54d5e26d47fbac6fd18eb3d
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2022-12-30 18:36:36 +0530

    Creating Back Button & Navigating Back On Button Click

--- app/src/main/java/prakhar/udemy/jetpackcompose/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/movieapp/screens/details/DetailsScreen.kt
@@ -1,14 +1,14 @@
 package prakhar.udemy.jetpackcompose.movieapp.screens.details
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -22,6 +22,12 @@ fun DetailsScreen(
             verticalArrangement = Arrangement.Center
         ) {
             Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
+            Spacer(modifier = Modifier.height(23.dp))
+            Button(onClick = {
+                navController.popBackStack()
+            }) {
+                Text(text = "Go Back")
+            }
         }
     }
 }
