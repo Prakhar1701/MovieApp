1d9ce5375b3a9ed7a90f5e99837fc953a8c9588d
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2022-12-30 11:57:51 +0530

    Starting using the Scaffold Composable

--- app/src/main/java/prakhar/udemy/jetpackcompose/movieapp/MainActivity.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/movieapp/MainActivity.kt
@@ -3,10 +3,10 @@ package prakhar.udemy.jetpackcompose.movieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import prakhar.udemy.jetpackcompose.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +23,16 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     MovieAppTheme {
-        content()
+        Scaffold(topBar = {
+            TopAppBar(
+                backgroundColor = Color.Magenta,
+                elevation = 5.dp
+            ) {
+                Text(text = "Movies")
+            }
+        }) {
+            content()
+        }
     }
 }
 
