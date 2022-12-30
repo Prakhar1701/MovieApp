bd62f9210404eee18fee673d0e4d1aceee12ca69
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2022-12-30 14:09:46 +0530

    Navigation Structure Setup

---
+++ app/src/main/java/prakhar/udemy/jetpackcompose/movieapp/navigation/MovieNavigation.kt
@@ -0,0 +1,16 @@
+package prakhar.udemy.jetpackcompose.movieapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MovieNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = MovieScreens.HomeScreen.name) {
+        composable(MovieScreens.HomeScreen.name) {
+            //Here we pass where this should lead us to
+        }
+    }
+}
