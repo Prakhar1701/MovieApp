ec0972cba0ebfb122b38c1eb1a1d3d68594f6734
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2022-12-30 12:13:06 +0530

    Checkpoint Commit

--- app/src/main/java/prakhar/udemy/jetpackcompose/movieapp/MainActivity.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/movieapp/MainActivity.kt
@@ -3,8 +3,13 @@ package prakhar.udemy.jetpackcompose.movieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import prakhar.udemy.jetpackcompose.movieapp.ui.theme.MovieAppTheme
@@ -37,8 +42,20 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun MainContent() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Hello Prakhar...")
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Avatar",
+        "300",
+        "Harry Potter",
+        "Life"
+    )
+) {
+
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(items = movieList) {
+                Text(text = it)
+            }
+        }
     }
 }
