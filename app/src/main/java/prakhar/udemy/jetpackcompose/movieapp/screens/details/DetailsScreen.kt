c5565988fc9dd8e4a8f9e199daf1200a93946806
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2022-12-31 15:34:10 +0530

    Worked On Details Screen

--- app/src/main/java/prakhar/udemy/jetpackcompose/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/movieapp/screens/details/DetailsScreen.kt
@@ -1,7 +1,10 @@
 package prakhar.udemy.jetpackcompose.movieapp.screens.details
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -9,14 +12,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
+import prakhar.udemy.jetpackcompose.movieapp.model.Movie
+import prakhar.udemy.jetpackcompose.movieapp.model.getMovies
+import prakhar.udemy.jetpackcompose.movieapp.widgets.MovieRow
 
 @Composable
 fun DetailsScreen(
     navController: NavController,
-    movieData: String?
+    movieId: String?
 ) {
+
+    val newMovieList = getMovies().filter { movie ->
+        movie.id == movieId
+    }
+
     Scaffold(topBar = {
         TopAppBar(
             backgroundColor = Color.Transparent,
@@ -37,9 +53,53 @@ fun DetailsScreen(
         Surface(modifier = Modifier.fillMaxSize()) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Top
             ) {
-                Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
+
+                MovieRow(movie = newMovieList.first())
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider()
+                Text(text = "Movie Images")
+                HorizontalScrollableImageView(newMovieList)
+            }
+        }
+    }
+}
+
+@Composable
+private fun HorizontalScrollableImageView(newMovieList: List<Movie>) {
+    LazyRow {
+        items(newMovieList[0].images) { image ->
+            Card(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(240.dp),
+                elevation = 5.dp
+            ) {
+
+//--------------------------------------------------------------------------------------------------
+                /* From:
+                https://coil-kt.github.io/coil/compose/  */
+
+                val painter = rememberAsyncImagePainter(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(image)
+//                                    .crossfade(true)
+//                                    .transformations(CircleCropTransformation())
+                        .size(Size.ORIGINAL) // Set the target size to load the image at.
+                        .build()
+                )
+
+                if (painter.state is AsyncImagePainter.State.Success) {
+                    // This will be executed during the first composition if the image is in the memory cache.
+                }
+
+                Image(
+                    painter = painter,
+                    contentDescription = "Movie Images"
+                )
+//--------------------------------------------------------------------------------------------------
+
             }
         }
     }
